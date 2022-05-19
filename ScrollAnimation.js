bb66a1079d79fd7c4095b6a6f020222a1ed53ba9
Giddf121 <55608701+Giddf121@users.noreply.github.com>
2022-05-19 13:07:30 -0400

    Update ScrollAnimation.js

--- ScrollAnimation.js
+++ ScrollAnimation.js
@@ -1,43 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import colors from './css-colors';
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
 
-import './styles.css';
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
 
-function FadeInSection(props) {
-  const [isVisible, setVisible] = React.useState(false);
-  const domRef = React.useRef();
-  React.useEffect(() => {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => setVisible(entry.isIntersecting));
-    });
-    observer.observe(domRef.current);
-  }, []);
-  return (
-    <div
-      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
-      ref={domRef}
-    >
-      {props.children}
-    </div>
-  );
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>All the CSS colors!</h1>
-
-      {colors.map(color => (
-        <FadeInSection key={color}>
-          <div className="box" style={{ backgroundColor: color }}>
-            <span>{color}</span>
-          </div>
-        </FadeInSection>
-      ))}
-    </div>
-  );
-}
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+window.addEventListener("scroll", reveal);
